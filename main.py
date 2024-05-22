3964e43c4eb34ceba872883da8b9286d18216d12
aasingh27 <aditsin1234567@gmail.com>
2024-05-22 19:20:15 -0400

    Main outline

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-# This is a test
-print("Hello world")
-
-=======
-print("he koichi")
-print("This is a test")
->>>>>>> 8e4c2dd9c8102801de3d31895e0746aaee25ebc1
+def main():
+    break
