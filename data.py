dc4560deedf7a1c762d653e084e5308000acf037
aasingh27 <aditsin1234567@gmail.com>
2024-05-22 22:20:42 -0400

    Version 1.0 datapy

---
+++ data.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://blog.prepscholar.com/list-of-ap-exams"
+r = requests.get(url)
+
+exams = []
+
+soup = BeautifulSoup(r.content, 'html.parser')
+results_all = soup.find_all("li")
+results_exam_name = results_all[:33]
+
+
+
+for exam in results_exam_name:
+    exam = str(exam)
+    clean_name = exam.lstrip("<li>").rstrip("</li>")
+    exams.append(clean_name)
+
+for name in exams:
+    print(name) 
